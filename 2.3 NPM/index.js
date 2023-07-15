d1b6f8599ea1e8463269874e5857da793403ff5b
SaboTaj22 <127702354+SaboTaj22@users.noreply.github.com>
2023-07-15 16:46:48 -0600

    Add files via upload

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,16 @@
+// Packages downloaded from https://www.npmjs.com/
+
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// Silly Names EX
+// import generateName from "sillyname";
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`); // Have to use backticks for this! Otherwise it won't work
+
+// Super Hero Name Exercise
+import superheroes from "superheroes"; // import superheroes package from superheroes
+
+const name = superheroes.random(); // Generate random super hero name
+console.log(`I am ${name} the super hero!`);
